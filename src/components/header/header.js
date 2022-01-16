e467a40525a90341ebef4a546295d7031b308680
xuqiming <tsian077@gmail.com>
2022-01-16 12:55:21 +0800

    redelploy

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -28,14 +28,14 @@ function Header(){
             <BrowserRouter>
            
    
-            <Navbar className="bg-light justify-content-center"  expand="lg" defaultActiveKey="/"> 
+            <Navbar className="bg-light justify-content-center"  expand="lg" defaultActiveKey="/yuki_project"> 
          
                 
                 {/* 首頁 */}
                 <Dropdown  className="d-inline-block" >
      
                   {/* caret  */}
-                  <DropdownToggle href="/"  className="">
+                  <DropdownToggle href="/yuki_project"  className="">
                     首頁
                   </DropdownToggle >
                 
@@ -54,7 +54,7 @@ function Header(){
               <Routes>
             
 
-                <Route path="/" element={<h1>hello</h1>} />
+                <Route path="/yuki_project" element={<h1>hello</h1>} />
                 <Route path="/introduction" element={<h1>hello introduction</h1>} />
                 <Route path="/class" element={<h1>hello class</h1>} />
 
